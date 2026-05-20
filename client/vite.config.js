a8d9e32b167fa0f088164c4699f4f829bb10649a
Gopal <dasgopal01978@gmail.com>
2026-05-20 18:52:51 +0530

    hccg

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: { '/api': { target: 'https://qwertyui-qzli.vercel.app', changeOrigin: true } }
+    proxy: { '/api': { target:  'https://qwertyui-qzli.vercel.app', changeOrigin: true } }
   }
 });
