2bbf5b3514e1029649e72a5f0d1c58a570e5d3b3
Gopal <dasgopal01978@gmail.com>
2026-05-20 18:38:26 +0530

    hccg

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
