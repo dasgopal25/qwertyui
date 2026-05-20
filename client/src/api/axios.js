90b55d354a252b22285769bc44370b91e7e0e3dd
Gopal <dasgopal01978@gmail.com>
2026-05-20 18:21:22 +0530

    hccg

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://server-prrj.onrender.com/api',
-});
+const api = axios.create({ baseURL: '/api' });
 
 api.interceptors.request.use((cfg) => {
   const token = localStorage.getItem('wc_token');
