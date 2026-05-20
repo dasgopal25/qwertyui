4717f02907663348e067550dabc6d8930e10a752
Gopal <dasgopal01978@gmail.com>
2026-05-20 17:15:15 +0530

    ghjhg

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://client1-0epa.onrender.com' });
 
 api.interceptors.request.use((cfg) => {
   const token = localStorage.getItem('wc_token');
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: { '/api': { target: 'http://localhost:5000', changeOrigin: true } }
+    proxy: { '/api': { target: 'https://client1-0epa.onrender.com', changeOrigin: true } }
   }
 });
